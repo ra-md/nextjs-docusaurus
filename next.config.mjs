9955c57f05a6bb547eeb9301688f3a7e97805170
ra-md <ra799m@gmail.com>
2024-03-08 10:39:19 +0700

    docusaurus integration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    rewrites: async () => {
+        const prod = [{
+            source: "/documentations",
+            destination: "/documentations/index.html",
+        }]
+
+        const dev = {
+            fallback: [
+                {
+                    source: '/:path*',
+                    destination: `http://localhost:3001/:path*`,
+                },
+            ],
+        }
+
+        return process.env.DEV ? dev : prod
+    },
+};
 
 export default nextConfig;
